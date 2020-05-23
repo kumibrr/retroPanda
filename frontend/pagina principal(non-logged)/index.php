b01df105d4df9037c185735f647eec14d6321c16
Israel <isra.lora99@gmail.com>
2020-05-23 02:49:56 +0200

    Código refactorizado

--- frontend/pagina principal(non-logged)/index.php
+++ frontend/pagina principal(non-logged)/index.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,29 +13,70 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css.css">
+    <link rel="stylesheet" href="../toolbar/toolbar.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-xl navbar-light bg-light fixed-top navbar-size shadow-sm">
+        <a class="navbar-brand" href="#"><img class="logo" src="../../assets/logo.png"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb">
+            <span class="navbar-toggler-icon"></span>
+        </button> 
+
+        <div class="collapse navbar-collapse bg-light" id="navb">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Explore</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">My Library</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Emulators</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto nav-flex-icons">
+                <form class="form-inline mr-5">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                </form>
+                <li class="nav-item mt-1 user-info">             
+                        <?php
+                            
+                            $userid = $_SESSION["user"];
+                            $servername = "localhost";
+                            $db_username = "root";
+                            $db_password = "";
+                            $db_name = "retropanda";
+
+                            $connection = mysqli_connect($servername,$db_username,$db_password,$db_name);
+
+                            $query = "SELECT USERNAME, IMAGEN_PERFIL FROM USUARIO WHERE ID_USUARIO = '$userid'";
+
+                            if($result = mysqli_query($connection,$query)){
+                                echo "<label class='nav-link'>";
+                                while ($row = mysqli_fetch_row($result)) {
+                                    echo "$row[0]<img src='$row[1]' class='rounded-circle z-depth-0 size1 ml-2' alt='avatar image'></label>";
+                                }
+                                echo "</label>";
+                            }
+                            
+                        ?>
+                    
+                </li>
+            </ul>
+        </div>
+    </nav>
+    
     
     <div style="text-align:center; padding-top:6%;border-bottom:green;">
     <h1  style="text-align:left;color:red">Wanted</h1>
         <?php
         // Datos de la base de datos
-        $usuario = "retropanda";
-        $password = "1dam";
-        $servidor = "localhost";
-        $basededatos = "retropanda";
-        
-        // creación de la conexión a la base de datos con mysql_connect()
-        $conexion = mysqli_connect( $servidor, $usuario, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
-
-        $resultado = mysqli_query( $conexion, "SET NAMES 'utf8'" ) or die ( "Algo ha ido mal en la consulta");
-
-        // Selección de la base de datos a utilizar
-        $db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
-
         $consulta = "SELECT * FROM JUEGO";
 
-        $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta $consulta");
+        $resultado = mysqli_query( $connection, $consulta ) or die ( "Algo ha ido mal en la consulta $consulta");
         while ($mostrar=mysqli_fetch_array($resultado)) {
             ?>
             <div style="float:left">
@@ -45,7 +90,6 @@
         
         <?php
         }
-        mysqli_close($conexion) or die("No se ha podido cerrar la conexión $conexion");
         ?>
 
 
@@ -54,26 +98,13 @@
     <div style="padding-top:7%;border-bottom:green;">
     <h1  style="text-align:left;color:red">Valoraciones</h1>
         <?php
-        // Datos de la base de datos
-        $usuario = "retropanda";
-        $password = "1dam";
-        $servidor = "localhost";
-        $basededatos = "retropanda";
-        
-        // creación de la conexión a la base de datos con mysql_connect()
-        $conexion = mysqli_connect( $servidor, $usuario, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
-
-        $resultado = mysqli_query( $conexion, "SET NAMES 'utf8'" ) or die ( "Algo ha ido mal en la consulta");
-
-        // Selección de la base de datos a utilizar
-        $db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
 
         $consulta = "
         SELECT usuario.USERNAME,juego.NOMBRE,valoracion_juego_usuario.VALORACION
         FROM `usuario`,`valoracion_juego_usuario`,`juego`
         WHERE valoracion_juego_usuario.ID_JUEGO=juego.ID_JUEGO AND valoracion_juego_usuario.ID_USUARIO=usuario.ID_USUARIO;";
 
-        $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta $consulta");
+        $resultado = mysqli_query( $connection, $consulta ) or die ( "Algo ha ido mal en la consulta $consulta");
         while ($mostrar=mysqli_fetch_array($resultado)) {
             ?>
             <div style="border-color: green;border-width: 7px;border-bottom-style: solid;">
@@ -88,7 +119,7 @@
         
         <?php
         }
-        mysqli_close($conexion) or die("No se ha podido cerrar la conexión $conexion");
+        mysqli_close($connection) or die("No se ha podido cerrar la conexión $conexion");
         ?>
 
     
