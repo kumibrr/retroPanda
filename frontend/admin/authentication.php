f5d00d1b542f4f37c143a029bdd4327bbb3e9d91
Israel <isra.lora99@gmail.com>
2020-05-23 19:30:41 +0200

    Frontend de página de administración

---
+++ frontend/admin/authentication.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+$DB_HOST = 'localhost';
+$DB_USER = 'root';
+$DB_PASSWORD = '';
+$DB_NAME = 'RETROPANDA';
+
+$connection = mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME);
+if(mysqli_connect_errno()){
+    exit('Failed to connect to database: ' . mysqli_connect_error());
+}
+
+if(!isset($_REQUEST['username'], $_REQUEST['password'])){
+    exit('Username or password are wrong');
+}
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+
+$query = "SELECT ID_USUARIO FROM USUARIO WHERE USUARIO.USERNAME = '$username' AND USUARIO.PASS = '$password'";
+
+if($result = mysqli_query($connection,$query)){
+    
+    $row = mysqli_num_rows($result);
+    
+    if($row == 1){
+        session_start();
+        while($fetch = mysqli_fetch_row($result)){
+            $_SESSION["admin"] = $fetch[0];
+        }
+        header("location: ../admin/adminp.php");
+    } else {
+        echo "El usuario o contraseña no son correctos.";
+        echo "<br><br><a href='login.html'>Volver</a>";
+    }
+} else{
+    echo "ha fallado algo :( " .mysqli_error($connection);
+}
+
+mysqli_close($connection);
+?>
---
+++ frontend/admin/loginadmin.js
@@ -0,0 +1,112 @@
+
+function main(){
+    let pupil_R = $('.pupil-r');
+    let pupil_L = $('.pupil-l');
+    let head = $('.p-head');
+    let inputValue = 0;
+
+    document.querySelectorAll('.form-control').forEach(item=>{
+        item.addEventListener('focus',()=>{
+            pupil_L.animate({
+                left: '151px',
+                top: '74px'
+            },200);
+
+            pupil_R.animate({
+                left: '61px',
+                top: '89px'
+            },200);
+
+            head.addClass('animateheadIn');
+            setTimeout(()=>{
+                head.addClass('pauseanimation');
+            },200)
+            
+        });
+
+        item.addEventListener('blur',()=>{
+            head.removeClass('pauseanimation');
+            setTimeout(()=>{
+                head.removeClass('animateheadIn');
+            },300)
+
+            pupil_L.animate({
+                left: '155px',
+                top: '67px'
+            },200);
+
+            pupil_R.animate({
+                left: '65px',
+                top: '76px'
+            },200);
+            
+        });
+
+        item.addEventListener('input',(e)=>{
+            let length = e.target.value.length;
+            if (length > inputValue && length < 8){
+                pupil_R.animate({
+                    left: '+=' + 1 + 'px',
+                    top: '+=' + 0.2 + 'px'
+                },100);
+
+                pupil_L.animate({
+                    left: '+=' + 0.2 + 'px',
+                    top: '+=' + 1 + 'px'
+                },100);
+
+            } else if(length > inputValue && length >= 4 && length < 24){
+                pupil_L.animate({
+                    left: '+=' + 1 + 'px'
+                },100);
+            } else if(length > inputValue){
+
+            } else{
+                pupil_R.animate({
+                    left: '-=' + 1 + 'px',
+                    top: '-=' + 0.2 + 'px'
+                },100);
+                pupil_L.animate({
+                    left: '-=' + 1 + 'px',
+                    top: '-=' + 0.2 + 'px'
+                },100);
+            }
+            inputValue = length;
+        });
+
+    });
+
+    document.querySelector('.modebtn').addEventListener('click',(e)=>{
+        e.preventDefault();
+        if($('.modebtn').hasClass('signupbtn')){
+            $('.login-c').addClass('signup-c').removeClass('login-c');
+            $('.login-form').addClass('signup-form').removeClass('login-form');
+            $('.loginsubmitbtn').addClass('signupsubmitbtn').removeClass('loginsubmitbtn');
+            $('.signupbtn').text('Log-in').addClass('loginbtn').removeClass('signupbtn');
+            document.querySelector('form').setAttribute('action','sign-up.php');
+            
+            document.querySelectorAll('.retype').forEach(item=>{
+                item.removeAttribute('hidden');
+            });
+        } else{
+            $('.signup-c').addClass('login-c').removeClass('signup-c');
+            $('.signup-form').addClass('login-form').removeClass('signup-form');
+            $('.signupsubmitbtn').addClass('loginsubmitbtn').removeClass('signupsubmitbtn');
+            $('.loginbtn').text('Sign-Up').addClass('signupbtn').removeClass('loginbtn');
+            document.querySelector('form').setAttribute('action','authentication.php');
+            
+            document.querySelectorAll('.retype').forEach(item=>{
+                item.setAttribute('hidden',null);
+            });
+        }
+
+        e.preventDefault();
+    });
+
+    document.querySelector('.commit').addEventListener('click',()=>{
+        document.querySelector('form').submit();
+    });
+
+}
+
+window.addEventListener('load',main);
